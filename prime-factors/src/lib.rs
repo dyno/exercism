7ea689b210672b5e07c2e83822e3893527986050
Dyno Fu <dfu@salesforce.com>
2024-12-27 09:26:20 -0800

    factors - 1

--- prime-factors/src/lib.rs
+++ prime-factors/src/lib.rs
@@ -1,3 +1,14 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+    // "This should calculate the prime factors of {n}"
+    let mut factors = Vec::new();
+    let mut n = n;
+    let mut factor = 2;
+    while n != 1 {
+        while n % factor == 0 {
+            factors.push(factor);
+            n /= factor;
+        }
+        factor += 1;
+    }
+    factors
 }
