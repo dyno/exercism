fde2a04a312aed760d31fe1345f63bcdfec26fd6
Dyno Fu <dfu@salesforce.com>
2024-12-27 08:28:53 -0800

    leap - boolean expression

--- leap/src/lib.rs
+++ leap/src/lib.rs
@@ -1,7 +1,3 @@
 pub fn is_leap_year(year: u64) -> bool {
-    match year {
-        _ if year % 4 != 0 => false,
-        _ if year % 100 == 0 => year % 400 == 0,
-        _ => true,
-    }
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
