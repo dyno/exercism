13cf5a41df513396fe2b34a2d4ef2f0cbac579b0
Dyno Fu <dfu@salesforce.com>
2024-12-27 20:34:41 -0800

    sieve: initialize

--- sieve/src/lib.rs
+++ sieve/src/lib.rs
@@ -1,11 +1,12 @@
 pub fn primes_up_to(upper_bound: u64) -> Vec<u64> {
-    let mut xs: Vec<_> = (0..=upper_bound).map(Option::from).collect();
-    let upper_bound = upper_bound as usize;
+    let mut xs = Vec::with_capacity(upper_bound as usize + 1);
+    xs.extend((0..=upper_bound).map(Some));
+    let upper = upper_bound as usize;
 
-    (2..=upper_bound)
+    (2..=upper)
         .filter_map(|i| {
             let p = xs[i].take()? as usize;
-            (p * p..=upper_bound).step_by(p).for_each(|j| xs[j] = None);
+            (p * p..=upper).step_by(p).for_each(|j| xs[j] = None);
             Some(p as u64)
         })
         .collect()
