53aa2b25a39cf7112b14d835b761c086e720986b
Dyno Fu <dfu@salesforce.com>
2024-12-27 21:07:36 -0800

    acronym: fold

--- acronym/src/lib.rs
+++ acronym/src/lib.rs
@@ -1,7 +1,17 @@
 pub fn abbreviate(phrase: &str) -> String {
-    phrase
-        .split(|c: char| c.is_whitespace() || c == '-' || c == '_')
-        .flat_map(|s| s.chars().filter(|c| c.is_alphabetic()).next())
-        .map(|c| c.to_ascii_uppercase())
-        .collect::<String>()
+    let (acc, _) = phrase
+        .chars()
+        .fold((String::new(), ' '), |(mut acc, prev), c| {
+            if c.is_alphabetic()
+                && (prev.is_whitespace()
+                    || prev == '-'
+                    || prev == '_'
+                    || (prev.is_lowercase() && c.is_uppercase()))
+            {
+                acc.push(c.to_ascii_uppercase());
+            }
+            (acc, c)
+        });
+
+    acc
 }
