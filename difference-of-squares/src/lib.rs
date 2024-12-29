8c82438dd0acd56860630d7ca122edaa3b835bc1
Dyno Fu <dfu@salesforce.com>
2024-12-28 21:04:58 -0800

    difference-of-squares: math solution

--- difference-of-squares/src/lib.rs
+++ difference-of-squares/src/lib.rs
@@ -1,10 +1,13 @@
 pub fn square_of_sum(n: u32) -> u32 {
-    let sum: u32 = (1..=n).sum();
+    let sum = n * (n + 1) / 2;
     sum * sum
 }
 
+
+// By observation and mathematical reasoning, the result of summing  
+// i^k (a k-th degree polynomial) is typically a (k+1)-th degree polynomial.
 pub fn sum_of_squares(n: u32) -> u32 {
-    (1..=n).map(|x| x * x).sum()
+    n * (n + 1) * (2 * n + 1) / 6
 }
 
 pub fn difference(n: u32) -> u32 {
