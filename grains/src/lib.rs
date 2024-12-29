3c1bc4354a655afad865bc9d27a0ab126650f577
Dyno Fu <dfu@salesforce.com>
2024-12-28 21:47:56 -0800

    grains: bit shifting

--- grains/src/lib.rs
+++ grains/src/lib.rs
@@ -1,9 +1,7 @@
 pub fn square(s: u32) -> u64 {
-    2u64.pow(s - 1)
+    1 << (s - 1)
 }
 
 pub fn total() -> u64 {
-    (1..=64_u32)
-        .map(square)
-        .sum()
+    ((1_u128 << 64) - 1) as u64
 }
