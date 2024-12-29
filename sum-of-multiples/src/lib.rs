63902210563cf5c58e2c4e0e19e4deea984a60c5
Dyno Fu <dfu@salesforce.com>
2024-12-28 22:30:08 -0800

    sum-of-multiples: by factors

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,5 +1,11 @@
+use std::collections::HashSet;
+
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    (1..limit)
-        .filter(|&n| factors.iter().any(|&factor| factor != 0 && n % factor == 0))
+    factors
+        .iter()
+        .filter(|&&factor| factor != 0)
+        .flat_map(|&factor| (factor..limit).step_by(factor as usize))
+        .collect::<HashSet<_>>()
+        .iter()
         .sum()
 }
