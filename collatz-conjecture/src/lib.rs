6f4d9aa0c1bef271666660d81f2308d178524d3f
Dyno Fu <dfu@salesforce.com>
2024-12-28 21:58:54 -0800

    collatz-conjecture

---
+++ collatz-conjecture/src/lib.rs
@@ -0,0 +1,18 @@
+pub fn collatz(n: u64) -> Option<u64> {
+    if n == 0 {
+        return None;
+    }
+
+    let mut n = n;
+    let mut count = 0;
+    while n != 1 {
+        if n % 2 == 0 {
+            n /= 2;
+        } else {
+            n = 3 * n + 1;
+        }
+        count += 1;
+    }
+
+    Some(count)
+}
